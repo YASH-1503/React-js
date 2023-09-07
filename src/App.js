446f2a014667d1e021a263eb4a836a4e337d66f3
YASH-1503 <jainy1601@gmail.com>
2023-09-07 19:21:38 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import Person from "./components/Person";
+// import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       vlnfubvoe
+//       <Person name="max" pop="bad">
+//         this is a propschiilderren in person tg
+//       </Person>
+//       <Person name="yash" pop="out of bound" />
+//       <Person name="jadav" pop="average" />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component, useState } from "react";
+import Person from "./components/Person";
+import "./App.css";
+
+const App = (props) => {
+  const [personstate, setpersonstate] = useState({
+    Person: [
+      { name: "yash", pop: "out of bound" },
+      { name: "max", pop: "bad" },
+      { name: "john", pop: "cascade" },
+    ],
+  });
+
+  const switchnamehandler = (newname) => {
+    //console.log("button clicked");
+    // this.setState(
+    //   { Person: [...this.state.Person].reverse() },
+    //   console.log("this")
+    // );
+    //this is to swap 1 and 3 sentence
+    setpersonstate({
+      Person: [
+        { name: newname, pop: "out of bound" },
+        { name: "max", pop: "bad" },
+        { name: "jahon", pop: "ouwevebt of dd" },
+      ],
+    });
+  };
+  inputnamehandler = (event) => {
+    setpersonstate({
+      Person: [
+        { name: "yash", pop: "out of bound" },
+        { name: event.target.value, pop: "bad" },
+        { name: "jahon", pop: "ouwevebt of dd" },
+      ],
+    });
+  };
+
+  {
+    return (
+      <div className="App">
+        <h1>Hello this is react learning season!!</h1>
+        <button onClick={switchnamehandler.bind(this, "rajesh")}>
+          switch button
+        </button>
+        <Person
+          name={personstate.Person[0].name}
+          pop={personstate.Person[0].pop}
+        />
+        <Person
+          name={personstate.Person[1].name}
+          pop={personstate.Person[1].pop}
+          this
+          is
+          outside
+          click={switchnamehandler.bind(this, "umesh")} //we can also sent method(switchnamehandler)as props
+          change={inputnamehandler}
+        ></Person>
+        <Person
+          name={personstate.Person[2].name}
+          pop={personstate.Person[2].pop}
+        ></Person>
+      </div>
+    );
+  }
+};
 
 export default App;
